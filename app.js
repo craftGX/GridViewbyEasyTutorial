dfeef7fcb0e8abe057932bb02c199933ac7d618b
stephane soussi <84611956+craftGX@users.noreply.github.com>
2021-09-15 16:40:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function full(){
+    document.getElementById('column1').style.width='100%'
+    document.getElementById('column2').style.width='100%'
+    document.getElementById('column3').style.width='100%'
+    document.getElementById('column4').style.width='100%'
+}
+function twoColumn(){
+    document.getElementById('column1').style.width='50%'
+    document.getElementById('column2').style.width='50%'
+    document.getElementById('column3').style.width='50%'
+    document.getElementById('column4').style.width='50%'
+}
+function fourColumn(){
+    document.getElementById('column1').style.width='25%'
+    document.getElementById('column2').style.width='25%'
+    document.getElementById('column3').style.width='25%'
+    document.getElementById('column4').style.width='25%'
+}
